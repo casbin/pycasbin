83af3f3f36c5abd14faa2e620481cfbef727a7ff
Marian Horban <mhorban@vidscale.com>
2019-02-26 16:00:02 +0200

    Change package name to egpcasbin

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="casbin",
+    name="egpcasbin",
     version="0.1.1",
     author="TechLee",
     author_email="techlee@qq.com",
     description="An authorization library that supports access control models like ACL, RBAC, ABAC in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/casbin/pycasbin",
+    url="https://github.com/VidScale/pycasbin",
     keywords=["casbin", "rbac", "access control", "abac", "acl", "permission"],
     packages=setuptools.find_packages(),
     install_requires=['simpleeval>=0.9.8'],
