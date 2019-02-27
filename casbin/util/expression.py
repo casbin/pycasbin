2f7d452474f7911822a8e0d671b4b95f78e6110d
Foufou <techlee@qq.com>
2019-02-27 10:31:00 +0800

    There is no need to judge whether it is None or not.

--- casbin/util/expression.py
+++ casbin/util/expression.py
@@ -2,10 +2,6 @@ from simpleeval import simple_eval
 
 
 def evaluate(exp_string, parameters=None, functions=None):
-    if parameters is None:
-        parameters = {}
-    if functions is None:
-        functions = {}
     exp_string = exp_string.replace("&&", "and")
     exp_string = exp_string.replace("||", "or")
     return simple_eval(exp_string, functions=functions, names=parameters)
