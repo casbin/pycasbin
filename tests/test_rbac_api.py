bc80af78883b802335d46b1b96908fda84b278bd
TechLee <techlee@qq.com>
2019-08-15 17:40:30 +0800

    Improve the test code for api.

--- tests/test_rbac_api.py
+++ tests/test_rbac_api.py
@@ -15,3 +15,53 @@ class TestRbacApi(TestCase):
         e = get_enforcer(get_examples("rbac_model.conf"), get_examples("rbac_policy.csv"))
 
         self.assertEqual(e.get_users_for_role('data2_admin'), ['alice'])
+
+    def test_has_role_for_user(self):
+        e = get_enforcer(get_examples("rbac_model.conf"), get_examples("rbac_policy.csv"))
+
+        self.assertTrue(e.has_role_for_user('alice', 'data2_admin'))
+        self.assertFalse(e.has_role_for_user('alice', 'data1_admin'))
+
+    def test_add_role_for_user(self):
+        e = get_enforcer(get_examples("rbac_model.conf"), get_examples("rbac_policy.csv"))
+        e.add_role_for_user('alice', 'data1_admin')
+        self.assertEqual(e.get_roles_for_user('alice'), ['data2_admin', 'data1_admin'])
+
+    def test_delete_role_for_user(self):
+        e = get_enforcer(get_examples("rbac_model.conf"), get_examples("rbac_policy.csv"))
+        e.add_role_for_user('alice', 'data1_admin')
+        self.assertEqual(e.get_roles_for_user('alice'), ['data2_admin', 'data1_admin'])
+
+        e.delete_role_for_user('alice', 'data1_admin')
+        self.assertEqual(e.get_roles_for_user('alice'), ['data2_admin'])
+
+    def test_delete_roles_for_user(self):
+        e = get_enforcer(get_examples("rbac_model.conf"), get_examples("rbac_policy.csv"))
+        e.delete_roles_for_user('alice')
+        self.assertEqual(e.get_roles_for_user('alice'), [])
+
+    def test_delete_user(self):
+        e = get_enforcer(get_examples("rbac_model.conf"), get_examples("rbac_policy.csv"))
+        e.delete_user('alice')
+        self.assertEqual(e.get_roles_for_user('alice'), [])
+
+    def test_delete_role(self):
+        e = get_enforcer(get_examples("rbac_model.conf"), get_examples("rbac_policy.csv"))
+        e.delete_role('data2_admin')
+        self.assertTrue(e.enforce('alice', 'data1', 'read'))
+        self.assertFalse(e.enforce('alice', 'data1', 'write'))
+        self.assertFalse(e.enforce('alice', 'data2', 'read'))
+        self.assertFalse(e.enforce('alice', 'data2', 'write'))
+        self.assertFalse(e.enforce('bob', 'data1', 'read'))
+        self.assertFalse(e.enforce('bob', 'data1', 'write'))
+        self.assertFalse(e.enforce('bob', 'data2', 'read'))
+        self.assertTrue(e.enforce('bob', 'data2', 'write'))
+
+    def test_delete_permission(self):
+        e = get_enforcer(get_examples("basic_without_resources_model.conf"),
+                         get_examples("basic_without_resources_policy.csv"))
+        e.delete_permission('read')
+        self.assertFalse(e.enforce('alice', 'read'))
+        self.assertFalse(e.enforce('alice', 'write'))
+        self.assertFalse(e.enforce('bob', 'read'))
+        self.assertTrue(e.enforce('bob', 'write'))
