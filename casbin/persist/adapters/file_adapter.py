f7f0a216c72ea23c88b3f10e32d84c27ec0700aa
Yang Luo <hsluoyz@qq.com>
2020-01-27 16:16:24 +0800

    Built-in file adapter supports save_policy() now.

--- casbin/persist/adapters/file_adapter.py
+++ casbin/persist/adapters/file_adapter.py
@@ -19,7 +19,10 @@ class FileAdapter(persist.Adapter):
         self._load_policy_file(model)
 
     def save_policy(self, model):
-        pass
+        if not os.path.isfile(self._file_path):
+            raise RuntimeError("invalid file path, file path cannot be empty")
+
+        self._save_policy_file(model)
 
     def _load_policy_file(self, model):
         with open(self._file_path, "rb") as file:
@@ -28,8 +31,25 @@ class FileAdapter(persist.Adapter):
                 persist.load_policy_line(line.decode().strip(), model)
                 line = file.readline()
 
-    def _save_policy_file(self, text):
-        pass
+    def _save_policy_file(self, model):
+        with open(self._file_path, "w") as file:
+            lines = []
+
+            if "p" in model.model.keys():
+                for key, ast in model.model["p"].items():
+                    for pvals in ast.policy:
+                        lines.append(key + ', ' + ', '.join(pvals))
+
+            if "g" in model.model.keys():
+                for key, ast in model.model["g"].items():
+                    for pvals in ast.policy:
+                        lines.append(key + ', ' + ', '.join(pvals))
+
+            for i, line in enumerate(lines):
+                if i != len(lines) - 1:
+                    lines[i] += "\n"
+
+            file.writelines(lines)
 
     def add_policy(self, sec, ptype, rule):
         pass
