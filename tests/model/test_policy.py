b113417014fbb98b49b651acb24987b59dc9e96f
Zxilly <zhouxinyu1001@gmail.com>
2021-02-02 17:45:34 +0800

    feat: add update_policy()
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>
    
    feat: further development on update_policy()
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>
    
    refactor: adjust to Python-style variable naming
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>
    
    feat: add update_policies()
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>
    
    feat: add unittest for update_policies()
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>
    
    refactor: remove adapter check
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>
    
    refactor: remove duplicated check
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- tests/model/test_policy.py
+++ tests/model/test_policy.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+
 from casbin.model import Model
 from tests.test_enforcer import get_examples
 
@@ -52,6 +53,43 @@ class TestPolicy(TestCase):
 
         self.assertTrue(m.get_policy('p', 'p') == [p_rule1, p_rule2])
         self.assertTrue(m.get_policy('g', 'g') == [g_rule])
+
+    def test_update_policy(self):
+        m = Model()
+        m.load_model(get_examples("basic_model.conf"))
+
+        old_rule = ['admin', 'domain1', 'data1', 'read']
+        new_rule = ['admin', 'domain1', 'data2', 'read']
+
+        m.add_policy('p', 'p', old_rule)
+        self.assertTrue(m.has_policy('p', 'p', old_rule))
+
+        m.update_policy('p', 'p', old_rule, new_rule)
+        self.assertFalse(m.has_policy('p', 'p', old_rule))
+        self.assertTrue(m.has_policy('p', 'p', new_rule))
+
+    def test_update_policies(self):
+        m = Model()
+        m.load_model(get_examples("basic_model.conf"))
+
+        old_rules = [['admin', 'domain1', 'data1', 'read'],
+                     ['admin', 'domain1', 'data2', 'read'],
+                     ['admin', 'domain1', 'data3', 'read']]
+        new_rules = [['admin', 'domain1', 'data4', 'read'],
+                     ['admin', 'domain1', 'data5', 'read'],
+                     ['admin', 'domain1', 'data6', 'read']]
+
+        m.add_policies('p', 'p', old_rules)
+
+        for old_rule in old_rules:
+            self.assertTrue(m.has_policy('p', 'p', old_rule))
+
+        m.update_policies('p', 'p', old_rules, new_rules)
+
+        for old_rule in old_rules:
+            self.assertFalse(m.has_policy('p', 'p', old_rule))
+        for new_rule in new_rules:
+            self.assertTrue(m.has_policy('p', 'p', new_rule))
 
     def test_remove_policy(self):
         m = Model()
