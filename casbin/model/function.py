bf51136ceaef1ccbcbf7b45d0ad4062a1b05679b
Jon Lee <techlee@qq.com>
2021-09-29 17:40:58 +0800

    fix: 'keyMatch4' not defined(#198) (#199)
    Signed-off-by: Jon Lee <techlee@qq.com>

--- casbin/model/function.py
+++ casbin/model/function.py
@@ -16,6 +16,7 @@ class FunctionMap:
         fm.add_function("keyMatch", util.key_match_func)
         fm.add_function("keyMatch2", util.key_match2_func)
         fm.add_function("keyMatch3", util.key_match3_func)
+        fm.add_function("keyMatch4", util.key_match4_func)
         fm.add_function("regexMatch", util.regex_match_func)
         fm.add_function("ipMatch", util.ip_match_func)
         fm.add_function("globMatch", util.glob_match_func)
