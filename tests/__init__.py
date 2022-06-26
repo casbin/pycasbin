8d83b8461af74b9dd39e2e9652921d969d735c7c
Yibo He <1137195420@qq.com>
2022-06-26 14:21:04 +0800

    fix: add import statement in tests module (#264)

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,3 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .test_distributed_api import TestDistributedApi
+from .test_enforcer import *
+from .test_filter import TestFilteredAdapter
+from .test_frontend import TestFrontend
+from .test_management_api import TestManagementApi, TestManagementApiSynced
+from .test_rbac_api import TestRbacApi, TestRbacApiSynced
+from . import benchmarks
+from . import config
+from . import model
+from . import rbac
+from . import util
