3d6adf6ca95ed41463a552ac7572c7432fac2573
Yibo He <1137195420@qq.com>
2022-06-26 14:21:04 +0800

    fix: add import statement in tests module (#264)

--- tests/util/__init__.py
+++ tests/util/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .test_builtin_operators import TestBuiltinOperators
+from .test_rwlock import TestRWLock
+from .test_util import TestUtil
