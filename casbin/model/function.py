7d29109d67d8f60c6be9fca6b4aaff523a8c156e
Haozhe Pan <haozhe.pan@outlook.com>
2023-08-30 12:42:09 +0800

    feat: add key_match5 function for matcher (#315)

--- casbin/model/function.py
+++ casbin/model/function.py
@@ -31,6 +31,7 @@ class FunctionMap:
         fm.add_function("keyMatch2", util.key_match2_func)
         fm.add_function("keyMatch3", util.key_match3_func)
         fm.add_function("keyMatch4", util.key_match4_func)
+        fm.add_function("keyMatch5", util.key_match5_func)
         fm.add_function("regexMatch", util.regex_match_func)
         fm.add_function("ipMatch", util.ip_match_func)
         fm.add_function("globMatch", util.glob_match_func)
