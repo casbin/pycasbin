4be7d9754ec6359fe1bf3a3cf2e4e5cdf7d3f7b6
BustDot <haozhe.pan@outlook.com>
2023-09-16 13:36:39 +0800

    fix: remove redundant init code

--- casbin/fast_enforcer.py
+++ casbin/fast_enforcer.py
@@ -12,33 +12,7 @@ class FastEnforcer(Enforcer):
 
     def __init__(self, model=None, adapter=None, enable_log=False, cache_key_order: Sequence[int] = None):
         self._cache_key_order = cache_key_order
-        self.logger = logging.getLogger("casbin.enforcer")
-        if isinstance(model, str):
-            if isinstance(adapter, str):
-                self.init_with_file(model, adapter)
-            else:
-                self.init_with_adapter(model, adapter)
-                pass
-        else:
-            if isinstance(adapter, str):
-                raise RuntimeError("Invalid parameters for enforcer.")
-            else:
-                self.init_with_model_and_adapter(model, adapter)
-
-        if enable_log:
-            configure_logging()
-
-    def init_with_file(self, model_path, policy_path):
-        """initializes an enforcer with a model file and a policy file."""
-        a = FileAdapter(policy_path)
-        self.init_with_adapter(model_path, a)
-
-    def init_with_adapter(self, model_path, adapter=None):
-        """initializes an enforcer with a database adapter."""
-        m = self.new_model(model_path)
-        self.init_with_model_and_adapter(m, adapter)
-
-        self.model_path = model_path
+        super().__init__(model, adapter, enable_log)
 
     def new_model(self, path="", text=""):
         """creates a model."""
@@ -52,16 +26,6 @@ class FastEnforcer(Enforcer):
             m.load_model_from_text(text)
 
         return m
-
-    def load_model(self):
-        """reloads the model from the model CONF file.
-        Because the policy is attached to a model, so the policy is invalidated and needs to be reloaded by calling LoadPolicy().
-        """
-
-        self.model = self.new_model()
-        self.model.load_model(self.model_path)
-        self.model.print_model()
-        self.fm = FunctionMap.load_function_map()
 
     def enforce(self, *rvals):
         """decides whether a "subject" can access a "object" with the operation "action",
