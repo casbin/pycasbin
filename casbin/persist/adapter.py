d3149e81c36d29e96ebdf3484897461ab86d8afb
Huon Wilson <huon@exoflare.io>
2025-06-07 23:03:11 +1000

    feat: Optimize `load_policy_line` to avoid quadratic individual-character loop (#355)

--- casbin/persist/adapter.py
+++ casbin/persist/adapter.py
@@ -12,34 +12,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
+
+_INTERESTING_TOKENS_RE = re.compile(r"[,\[\]\(\)]")
+
+
+def _extract_tokens(line):
+    """Return the list of 'tokens' from the line, or None if this line has none"""
+
+    if line == "":
+        return None
+
+    if line[:1] == "#":
+        return None
+
+    stack = []
+    tokens = []
+
+    # The tokens are separated by commas, but we support nesting so a naive `line.split(",")` is
+    # wrong. E.g. `abc(def, ghi), jkl` is two tokens: `abc(def, ghi)` and `jkl`. We do this by
+    # iterating over the locations of any tokens of interest, and either:
+    #
+    # - [](): adjust the nesting depth
+    # - ,: slice the line to save the token, if the , is at the top-level, outside all []()
+    #
+    # `start_idx` represents the start of the current token, that we haven't seen a `,` for yet.
+    start_idx = 0
+    for match in _INTERESTING_TOKENS_RE.finditer(line):
+        c = match.group()
+        if c == "[" or c == "(":
+            stack.append(c)
+        elif c == "]" or c == ")":
+            stack.pop()
+        elif not stack:
+            # must be a comma outside of any nesting: we've found the end of a top level token so
+            # save that and start a new one
+            tokens.append(line[start_idx : match.start()].strip())
+            start_idx = match.end()
+
+    # trailing token after the last ,
+    tokens.append(line[start_idx:].strip())
+
+    return tokens
+
 
 def load_policy_line(line, model):
     """loads a text line as a policy rule to model."""
 
-    if line == "":
+    tokens = _extract_tokens(line)
+    if tokens is None:
         return
-
-    if line[:1] == "#":
-        return
-
-    stack = []
-    tokens = []
-    for c in line:
-        if c == "[" or c == "(":
-            stack.append(c)
-            tokens[-1] += c
-        elif c == "]" or c == ")":
-            stack.pop()
-            tokens[-1] += c
-        elif c == "," and len(stack) == 0:
-            tokens.append("")
-        else:
-            if len(tokens) == 0:
-                tokens.append(c)
-            else:
-                tokens[-1] += c
-
-    tokens = [x.strip() for x in tokens]
 
     key = tokens[0]
     sec = key[0]
