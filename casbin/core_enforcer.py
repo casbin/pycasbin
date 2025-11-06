dbd28b728c617c28f34478bd9dde3f712fc1c37e
Yang Luo <hsluoyz@gmail.com>
2025-11-06 20:58:39 +0800

    feat: fix enforce_ex() API returning empty explanations for allow rules in deny models (#412)

--- casbin/core_enforcer.py
+++ casbin/core_enforcer.py
@@ -481,8 +481,11 @@ class CoreEnforcer:
                 else:
                     policy_effects.add(Effector.ALLOW)
 
+                # Update explain_index for any matching policy before checking early break condition
+                # to ensure explanations are captured for allow rules in deny models
+                explain_index = i
+
                 if self.eft.intermediate_effect(policy_effects) != Effector.INDETERMINATE:
-                    explain_index = i
                     break
 
         else:
